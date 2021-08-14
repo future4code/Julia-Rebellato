00fb62d51e06ebc8381fc2a2ce575d89a0079131
Julia Zielke Rebellato <juliazrebellato@gmail.com>
2021-08-14 16:05:01 -0300

    ate-a-13

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -145,16 +145,52 @@ function segundoMaiorEMenor(array) {
 
 // EXERCÍCIO 11
 function ordenaArray(array) {
+    const len = array.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (array[j] > array[j + 1]) {
+                let tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+    return array;
+};
 
-}
 
 // EXERCÍCIO 12
 function filmeFavorito() {
-
+  let filme = {
+    nome: "O Diabo Veste Prada",
+    ano: 2006,
+    diretor: "David Frankel",
+    atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"]
+  }
+  return filme
 }
 
 // EXERCÍCIO 13
 function imprimeChamada() {
+
+  let filme = {
+    nome: "O Diabo Veste Prada",
+    ano: 2006,
+    diretor: "David Frankel",
+    atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"]
+  }
+
+  let atoresFrase = ""
+  for(i = 0; i < filme.atores.length; i++) {
+    if (i === filme.atores.length - 1) {
+      atoresFrase += filme.atores[i]
+    } else {
+      atores += filmes.atores[i] + ","
+    }
+  }
+
+ return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estraldo por ${atoresFrase}`
+ 
   // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
 }
 
