8430d36617571e65c4270e6efc16ba4689bf1b95
Julia Zielke Rebellato <juliazrebellato@gmail.com>
2021-08-14 08:50:12 -0300

    alterar-revisao-js

---
+++ quinzena3/revisao-js/exercicios.js
@@ -0,0 +1,153 @@
+// EXERCÍCIO 01
+function inverteArray(array) {
+  let numeros = []
+
+  for(let i = array.length - 1; i >= 0; i--){
+    numeros.push(array[i])
+  }
+
+return numeros
+}
+
+// EXERCÍCIO 02
+function retornaNumerosParesElevadosADois(array) {
+  let numerosPares = array.filter(n => n % 2 === 0)
+
+  let numerosParesAoQuadrado = numerosPares.map(n => n **2)
+
+  return numerosParesAoQuadrado
+}
+
+// EXERCÍCIO 03
+function retornaNumerosPares(array) {
+let arrayPares = []
+
+for(i =0; i < array.length; i++){
+  if(array[i] % 2 === 0)
+  arrayPares.push(array[i])
+}
+ return arrayPares
+}
+
+// EXERCÍCIO 04
+function retornaMaiorNumero(array) {
+  let maiorNumero = 0
+  for( let i = 0; i < array.length; i++){
+    if(array[i] > maiorNumero){
+      maiorNumero = array[i]
+    }
+  }
+return maiorNumero
+}
+
+// EXERCÍCIO 05
+function retornaQuantidadeElementos(array) {
+  
+}
+
+// EXERCÍCIO 06
+function retornaExpressoesBooleanas() {
+
+}
+
+// EXERCÍCIO 07
+function retornaNNumerosPares(n) {
+
+}
+
+// EXERCÍCIO 08
+function checaTriangulo(a, b, c) {
+  // return 'Escaleno'
+  // return 'Equilátero'
+  // return 'Isósceles'
+}
+
+// EXERCÍCIO 09
+function comparaDoisNumeros(num1, num2) {
+  // Formato do objeto a ser retornado:
+  // {
+  //   maiorNumero: X,
+  //   maiorDivisivelPorMenor: Y,
+  //   diferenca: Z
+  // }
+}
+
+// EXERCÍCIO 10
+function segundoMaiorEMenor(array) {
+
+}
+
+// EXERCÍCIO 11
+function ordenaArray(array) {
+
+}
+
+// EXERCÍCIO 12
+function filmeFavorito() {
+
+}
+
+// EXERCÍCIO 13
+function imprimeChamada() {
+  // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+}
+
+// EXERCÍCIO 14
+function criaRetangulo(lado1, lado2) {
+
+}
+
+// EXERCÍCIO 15
+function anonimizaPessoa(pessoa) {
+
+}
+
+// EXERCÍCIO 16A
+function maioresDe18(arrayDePessoas) {
+
+}
+
+// EXERCÍCIO 16B
+function menoresDe18(arrayDePessoas) {
+
+}
+
+// EXERCÍCIO 17A
+function multiplicaArrayPor2(array) {
+
+}
+
+// EXERCÍCIO 17B
+function multiplicaArrayPor2S(array) {
+
+}
+
+// EXERCÍCIO 17C
+function verificaParidade(array) {
+
+}
+
+// EXERCÍCIO 18A
+function retornaPessoasAutorizadas(pessoas) {
+
+}
+
+// EXERCÍCIO 18B
+function retornaPessoasNaoAutorizadas(pessoas) {
+
+}
+
+// EXERCÍCIO 19A
+function ordenaPorNome(consultasNome) {
+
+}
+
+// EXERCÍCIO 19B
+function ordenaPorData(consultasData) {
+
+}
+
+// EXERCÍCIO 20
+function calculaSaldo(contas) {
+
+}
