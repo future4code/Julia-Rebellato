5c700ff48bdc7565a07ea2426f46eb09d74cff7d
Julia Zielke Rebellato <juliazrebellato@gmail.com>
2021-10-15 23:02:49 -0300

    labefy início

---
+++ quinzena7/projeto/labefy/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+export default class App extends React.Component {
+  render(){
+  return (
+<h1>LABEFY</h1>
+  )}
+  }
+
